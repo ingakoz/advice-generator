6b98ce9295d56dbfa230786f8cfc77da1ceeb52c
Inga Kozlowska <81585384+ingakoz@users.noreply.github.com>
2021-06-01 18:13:35 +0200

    generator created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { advice: ''};
+
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const {advice} = response.data.slip;
+      const {id} = response.data.slip;
+
+      console.log(id);
+        this.setState ({ advice:advice });
+        this.setState({ id:id });
+       
+      
+    })
+    .catch ((error) => {
+
+    });
+  }
+
+  render(){
+    const  {advice} = this.state;
+    const {id} = this.state;
+    return(  
+      
+      <div className="App">
+      <h1 className="Name">Absolutely Fucking Useless <br />Advice Generator</h1>
+        <div className="Card">
+         <h4 className="id">Advice #{id}</h4>
+      <h1 className="Head">{advice}</h1> 
+      <button className="Button" onClick={this.fetchAdvice}>
+        <span>Next Advice</span>
+      </button>
+      </div>
+      </div>  
+    )
+  }
 }
+
 
 export default App;
